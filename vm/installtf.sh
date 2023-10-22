7c974eee13e8d9a0216c4efcbdf348acfc0c6ad8
AS2Technologies <as2technologies.training@gmail.com>
2023-10-23 01:02:40 +0530

    Update installtf.sh

--- vm/installtf.sh
+++ vm/installtf.sh
@@ -3,4 +3,7 @@ wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sha
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 
-https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_darwin_amd64.zip
+https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_linux_386.zip
+https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_linux_amd64.zip
+https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_linux_arm.zip
+https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_linux_arm64.zip
